900f67be81c5cc803869ba94349db9efc2f33fd0
walkerlala <awhitetshirt@163.com>
2016-07-21 12:16:29 +0800

    add test

---
+++ spider/test/client.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import socket
+import threading
+import sys
+import os
+import time
+
+TCP_IP = '119.29.166.19'
+#TCP_IP = '116.56.143.101'
+#TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+
+socket.setdefaulttimeout(30)
+
+client_log = open("client_log.log","w+")
+
+def send_to_server(links):
+    try:
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect((TCP_IP,TCP_PORT))
+        for link in links:
+            try:
+                sock.sendall(link)
+            except Exception as e:
+                client_log.write("Error: %s: %s\n" % (str(e),str(link)))
+                continue
+            client_log.write("Successfully write one link: %s\n", str(link))
+            time.sleep(1)
+        sock.close()
+    except Exception as e:
+        client_log.write("Error: " + str(e) + "\n")
+
+thread_list = []
+with open("links.links","r") as f:
+    tmp_list = []
+    for count, link in enumerate(f):
+        if(len(thread_list) >= 10):
+            for thread in thread_list:
+                thread.join()
+            thread_list = []
+        if count % 100 != 0:
+            tmp_list.append(link)
+        else:
+            t = threading.Thread(target=send_to_server,args=(tmp_list,))
+            thread_list.append(t)
+            tmp_list=[]
+            t.start()
+
+#remove empty client_log
+if (os.path.getsize(client_log) == 0):
+    os.remove(client_log)
+
---
+++ spider/test/server.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import socket
+import sys
+import threading
+import os
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = 5005
+#BUFFER_SIZE should be power of 2 to speed up(hardware reason)
+BUFFER_SIZE = 512
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind((TCP_IP,TCP_PORT))
+#listen up to 5 client(what if that exceed?)
+sock.listen(5)
+
+server_log = open("server_log.log","w+")
+
+def handle_connection(conn,file_handle):
+    while(True):
+        try:
+            data = conn.recv(BUFFER_SIZE)
+            if data:
+                file_handle.write(data)
+                file_handle.flush()
+            else:
+                file_handle.close()
+                conn.close()
+                break
+        except Exception as e:
+            server_log.write("Error: %s\n" % str(e))
+            print >>sys.stderr, "Error: ", str(e)
+            continue
+
+# dir store all the links file
+dir = "dir"
+if not os.path.exists(dir):
+    os.mkdir(dir)
+else:
+    os.remove(dir)
+
+#The connection is actually another socket assigned by the kernel
+#we are not handling the situation where there are too many connection requests
+thread_list = []
+counter = 0
+while True:
+    # 10 thread at a time
+    if len(thread_list) > 10:
+        for thread in thread_list:
+            thread.join()
+        thread_list = []
+    conn, addr = sock.accept()
+    server_log.write("connection address: %s\n" % str(addr))
+    try:
+        file_handle = open(dir + "/" + "server_recv." + str(counter),"w+")
+        counter = counter + 1
+        t = threading.Thread(target=handle_connection,args=(conn,file_handle));
+        thread_list.append(t)
+        t.start()
+    except Exception as e:
+        server_log.write("Error: " + str(e) + "\n")
+
+#remove empty log file
+if (os.path.getsize(server_log) == 0):
+    os.remove(server_log)
+
+
