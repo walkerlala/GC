2c476217e3b6550a62856309a4bad832deb21017
ChelyYi <197163391@qq.com>
2016-12-01 18:43:03 +0800

    Add files via upload
    add the data structure which is the simplified Record and RecordPool type for comparing.

---
+++ classifier/src/cn/lasagna/www/classifier/CompareRecord.java
@@ -0,0 +1,60 @@
+package cn.lasagna.www.classifier;
+
+import java.util.HashMap;
+
+public class CompareRecord {
+	private String page_id;
+	private String tag;
+	private HashMap<String,Double> keywordMap;
+	private HashMap<String,Double> titleMap;
+	private HashMap<String,Double> descriptionMap;
+	
+	public CompareRecord(){
+		this.page_id = null;
+		this.tag = null;
+		this.keywordMap = new HashMap<String,Double>();
+		this.titleMap = new HashMap<String,Double>();
+		this.descriptionMap = new HashMap<String,Double>();
+	}
+	
+	public void setPage_id(String id){
+		this.page_id = id;
+	}
+	
+	public void setTag(String tag){
+		this.tag = tag;
+	}
+	
+	public void setKeywordMap(HashMap<String,Double> keyword){
+		this.keywordMap = keyword;
+	}
+	
+	public void setTitleMap(HashMap<String,Double> title){
+		this.titleMap = title;
+	}
+	
+	public void setDescriptonMap(HashMap<String,Double> description){
+		this.descriptionMap = description;
+	}
+	
+	public String getPage_id(){
+		return page_id;
+	}
+	
+	public String getTag(){
+		return tag;
+	}
+	
+	public HashMap<String,Double> getKeyword(){
+		return keywordMap;
+	}
+	
+	public HashMap<String,Double> getTitle(){
+		return titleMap;
+	}
+	
+	public HashMap<String,Double> getDescripton(){
+		return descriptionMap;
+	}
+
+}
